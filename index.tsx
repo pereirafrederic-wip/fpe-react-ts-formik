65d4fcb581a6d880182ac54a3fbcd376a449a71d
pereirafrederic <pereira.frederic.developpeur@gmail.com>
2019-06-01 11:46:02 +0200

    1 file modified, 1 added

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import Basic from './Basic';
 import './style.css';
 
 interface AppProps { }
@@ -19,10 +19,7 @@ class App extends Component<AppProps, AppState> {
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <Basic />
       </div>
     );
   }
